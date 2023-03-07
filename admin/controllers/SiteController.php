eef5bbf33740c68b34aa9003881993cdfb2e7abc
= <=>
2023-03-07 14:24:05 +0700

    Perbaikan access

--- admin/controllers/SiteController.php
+++ admin/controllers/SiteController.php
@@ -20,20 +20,24 @@ class SiteController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'actions' => ['login', 'error'],
-                        'allow' => true,
-                    ],
-                    [
-                        'actions' => ['logout', 'index'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
+
+
+            //bentrok dengan mdm
+            // 'access' => [
+            //     'class' => AccessControl::className(),
+            //     'rules' => [
+            //         [
+            //             'actions' => ['login', 'error'],
+            //             'allow' => true,
+            //         ],
+            //         [
+            //             'actions' => ['logout', 'index'],
+            //             'allow' => true,
+            //             'roles' => ['@'],
+            //         ],
+            //     ],
+            // ],
+
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
--- admin/views/siswa/index.php
+++ admin/views/siswa/index.php
@@ -19,71 +19,73 @@ CrudAsset::register($this);
 ?>
 
 <div class="siswa-index">
-    <div id="ajaxCrudDatatable">
-        <?= GridView::widget([
-            'id' => 'crud-datatable',
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'pjax' => true,
-            'columns' => require(__DIR__ . '/_columns.php'),
-            'toolbar' => [
-                [
-                    'content' =>
-                    Html::a(
-                        '<i class="fas fa fa-plus" aria-hidden="true"></i>',
-                        ['create'],
-                        ['role' => 'modal-remote', 'title' => 'Tambah Siswas', 'class' => 'btn btn-default']
-                    ) .
-                        Html::a(
-                            '<i class="fas fa fa-sync" aria-hidden="true"></i>',
-                            [''],
-                            ['data-pjax' => 1, 'class' => 'btn btn-default', 'title' => 'Reset Grid']
-                        ) .
-                        '{toggleData}' .
-                        '{export}'
-                ],
-            ],
-            'striped' => true,
-            'condensed' => true,
-            'responsive' => true,
-            'panel' => [
-                'type' => 'primary',
-                'heading' => '<i class="fas fa fa-list" aria-hidden="true"></i> Siswas listing',
-                'before' =>
-                Html::a(
-                    '+ Tambah',
-                    ['create'],
+    <div class="table-responsive signal-table">
+        <div id="ajaxCrudDatatable">
+            <?= GridView::widget([
+                'id' => 'crud-datatable',
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'pjax' => true,
+                'columns' => require(__DIR__ . '/_columns.php'),
+                'toolbar' => [
                     [
-                        'role' => 'modal-remote',
-                        'title' => 'Create new Siswas',
-                        'class' => 'btn btn-warning'
-                    ]
-                ),
-                'after' => BulkButtonWidget::widget([
-                    'buttons' => Html::a(
-                        '<i class="fas fa fa-trash" aria-hidden="true"></i>&nbsp; Hapus semua',
-                        ["bulkdelete"],
+                        'content' =>
+                        Html::a(
+                            '<i class="fas fa fa-plus" aria-hidden="true"></i>',
+                            ['create'],
+                            ['role' => 'modal-remote', 'title' => 'Tambah Siswas', 'class' => 'btn btn-default']
+                        ) .
+                            Html::a(
+                                '<i class="fas fa fa-sync" aria-hidden="true"></i>',
+                                [''],
+                                ['data-pjax' => 1, 'class' => 'btn btn-default', 'title' => 'Reset Grid']
+                            ) .
+                            '{toggleData}' .
+                            '{export}'
+                    ],
+                ],
+                'striped' => false,
+                'condensed' => false,
+                'responsive' => true,
+                'hover' => true,
+                'panel' => [
+                    'type' => 'primary',
+                    'heading' => '<i class="fas fa fa-list" aria-hidden="true"></i> Siswas listing',
+                    'before' =>
+                    Html::a(
+                        '+ Tambah',
+                        ['create'],
                         [
-                            "class" => "btn btn-danger btn-xs",
-                            'role' => 'modal-remote-bulk',
-                            'data-confirm' => false, 'data-method' => false, // for overide yii data api
-                            'data-request-method' => 'post',
-                            'data-confirm-title' => 'Aapakah anda yakin?',
-                            'data-confirm-message' => 'Apakah Anda yakin akan menghapus data ini?'
+                            'role' => 'modal-remote',
+                            'title' => 'Create new Siswas',
+                            'class' => 'btn btn-warning'
                         ]
                     ),
-                ]) .
-                    '<div class="clearfix"></div>',
-            ]
-        ]) ?>
+                    'after' => BulkButtonWidget::widget([
+                        'buttons' => Html::a(
+                            '<i class="fas fa fa-trash" aria-hidden="true"></i>&nbsp; Hapus semua',
+                            ["bulkdelete"],
+                            [
+                                "class" => "btn btn-danger btn-xs",
+                                'role' => 'modal-remote-bulk',
+                                'data-confirm' => false, 'data-method' => false, // for overide yii data api
+                                'data-request-method' => 'post',
+                                'data-confirm-title' => 'Aapakah anda yakin?',
+                                'data-confirm-message' => 'Apakah Anda yakin akan menghapus data ini?'
+                            ]
+                        ),
+                    ]) .
+                        '<div class="clearfix"></div>',
+                ]
+            ]) ?>
+        </div>
     </div>
-</div>
-<?php Modal::begin([
-    "options" => [
+    <?php Modal::begin([
+        "options" => [
+            "id" => "ajaxCrudModal",
+            "tabindex" => false // important for Select2 to work properly
+        ],
         "id" => "ajaxCrudModal",
-        "tabindex" => false // important for Select2 to work properly
-    ],
-    "id" => "ajaxCrudModal",
-    "footer" => "", // always need it for jquery plugin
-]) ?>
-<?php Modal::end(); ?>
+        "footer" => "", // always need it for jquery plugin
+    ]) ?>
+    <?php Modal::end(); ?>
