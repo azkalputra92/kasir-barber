4520727a21e3d3ef06b78811db276e897717354d
= <=>
2023-03-07 15:47:31 +0700

    konfigurasi summary & linkPager

--- admin/views/siswa/_form.php
+++ admin/views/siswa/_form.php
@@ -9,51 +9,49 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="siswa-form">
-    <div class="row">
-
-        <?php if (!Yii::$app->request->isAjax) { ?>
-            <div class="card">
-                <div class="card-body">
 
 
+    <?php if (!Yii::$app->request->isAjax) { ?>
+        <div class="card">
+            <div class="card-body">
 
-                    <?php $form = ActiveForm::begin(); ?>
+                <?php $form = ActiveForm::begin(); ?>
 
-                    <?= $form->field($model, 'nis')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'nis')->textInput(['maxlength' => true]) ?>
 
-                    <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
 
-                    <?= $form->field($model, 'alamat')->textarea(['rows' => 6]) ?>
+                <?= $form->field($model, 'alamat')->textarea(['rows' => 6]) ?>
 
-                    <?= $form->field($model, 'id_user')->textInput() ?>
-
-
-                    <div class="form-group mt-3">
-                        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-                    </div>
+                <?= $form->field($model, 'id_user')->textInput() ?>
 
 
-                    <?php ActiveForm::end(); ?>
-                </div>
-            </div>
-        <?php } else { ?>
-
-            <?php $form = ActiveForm::begin(); ?>
-
-            <?= $form->field($model, 'nis')->textInput(['maxlength' => true]) ?>
-
-            <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
-
-            <?= $form->field($model, 'alamat')->textarea(['rows' => 6]) ?>
-
-            <?= $form->field($model, 'id_user')->textInput() ?>
-
-            <?php if (!Yii::$app->request->isAjax) { ?>
                 <div class="form-group mt-3">
                     <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
                 </div>
-            <?php } ?>
-            <?php ActiveForm::end(); ?>
+
+
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    <?php } else { ?>
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'nis')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'nama')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'alamat')->textarea(['rows' => 6]) ?>
+
+        <?= $form->field($model, 'id_user')->textInput() ?>
+
+        <?php if (!Yii::$app->request->isAjax) { ?>
+            <div class="form-group mt-3">
+                <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+            </div>
         <?php } ?>
-    </div>
+        <?php ActiveForm::end(); ?>
+    <?php } ?>
+
 </div>
