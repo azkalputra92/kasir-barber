679b092a3c3c739291f6ad233a0fc4f09cce50d7
= <=>
2023-03-10 11:14:54 +0700

    Perbaikan site/index

--- admin/controllers/SiteController.php
+++ admin/controllers/SiteController.php
@@ -67,7 +67,13 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $searchModel = new SiswaSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
     /**
